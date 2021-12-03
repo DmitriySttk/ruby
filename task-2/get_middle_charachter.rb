110015eed54392f1b17c802ecbbe1489d647d099
faceroll <set.frnx@gmail.com>
2021-12-03 16:24:34 +0300

    ref

--- task-2/get_middle_charachter.rb
+++ task-2/get_middle_charachter.rb
@@ -1,12 +1,12 @@
 #!/usr/bin/env ruby
-MESSAGE = "Enter word"
+MESSAGE = "enter word"
 EXIT_MESSAGE = "exit!"
 ERROR_MESSAGE = "can't be blank"
 
 def get_middle(word)
   length = word.length
-  puts word[length / 2] if length % 2 != 0
-  puts "#{word[length / 2 - 1]}#{word[length / 2]}" if length % 2 == 0
+  puts "middle: #{word[length / 2]}" if length % 2 != 0
+  puts "middle: #{word[length / 2 - 1]}#{word[length / 2]}" if length % 2 == 0
 end
 
 def run
@@ -14,7 +14,7 @@ def run
     puts MESSAGE
     word = gets.chomp
     break if word == EXIT_MESSAGE
-    word.empty? ? (puts ERROR_MESSAGE) : (puts get_middle(word))
+    word.empty? ? (puts ERROR_MESSAGE) : (get_middle(word))
   end
 end
 
