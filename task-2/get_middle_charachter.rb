5b7d1aca50cf89a8309371c95f1199aea7619a98
faceroll <set.frnx@gmail.com>
2021-12-14 11:36:04 +0300

    get_middle refactored

--- task-2/get_middle_charachter.rb
+++ task-2/get_middle_charachter.rb
@@ -5,8 +5,7 @@ ERROR_MESSAGE = "can't be blank"
 
 def get_middle(word)
   length = word.length
-  puts "middle: #{word[length / 2]}" if length % 2 != 0
-  puts "middle: #{word[length / 2 - 1]}#{word[length / 2]}" if length % 2 == 0
+  word.length.even? ? "middle: #{word[length / 2 - 1]}#{word[length / 2]}" : "middle: #{word[length / 2]}"
 end
 
 def run
@@ -14,7 +13,8 @@ def run
     puts MESSAGE
     word = gets.chomp
     break if word == EXIT_MESSAGE
-    word.empty? ? (puts ERROR_MESSAGE) : (get_middle(word))
+    puts word.empty? ? (puts ERROR_MESSAGE) : (get_middle(word))
+
   end
 end
 
