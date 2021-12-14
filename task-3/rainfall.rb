ac98a6bbdc48f0a82e4aaaf0029f8ad5c65f982e
faceroll <set.frnx@gmail.com>
2021-12-14 16:36:14 +0300

    description of added methods

--- task-3/rainfall.rb
+++ task-3/rainfall.rb
@@ -1,9 +1,53 @@
 #!/usr/bin/env ruby
 
-def mean(town, strng)
+WELCOME_MESSAGE = "Enter city name: "
+ERROR_MESSAGE = "City name can not be blank!"
+EXIT_MESSAGE = "exit!"
+NUMBER_OF_MONTHS = 12
 
+def get_data
+  file = File.new("/home/faceroll/RubymineProjects/ruby/task-3/data") #It doesn't work as 'data' or 'task-3/data.txt' ¯\_(ツ)_/¯
+  file.read
 end
 
-def variance(town, strng)
-
+def mean(town, data)
+  if data.include?(town)
+    weather_data(town, data).sum / NUMBER_OF_MONTHS
+  else
+    -1
+  end
 end
+
+def variance(town, data)
+  if data.include?(town)
+    weather_data(town, data).map { |city| (mean(town, data) - city) ** 2 }.sum / NUMBER_OF_MONTHS
+  else
+    -1
+  end
+end
+
+def weather_data(town, data)
+
+  monthly_rainfall = data.split("\n").find { |data_row| data_row.match "#{town}:" }
+
+  return if monthly_rainfall.nil?
+  monthly_rainfall.scan(/\d*\.\d/).map(&:to_f)
+end
+
+def run_cli
+  loop do
+    puts WELCOME_MESSAGE
+    entered_string = gets.chomp
+    break if entered_string == EXIT_MESSAGE
+
+    entered_string.empty? ? puts(ERROR_MESSAGE) : show_result(entered_string)
+
+  end
+end
+
+def show_result(input)
+  puts "Rainfall mean: #{mean(input, get_data)}"
+  puts "Rainfall variance: #{variance(input, get_data)}"
+end
+
+run_cli
