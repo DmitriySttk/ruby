30963329646382a48d62a4a8030a667112007469
faceroll <set.frnx@gmail.com>
2022-01-04 17:11:38 +0300

    init commit

--- task-6/scraper.rb
+++ task-6/scraper.rb
@@ -13,9 +13,9 @@ Capybara.configure do |config|
   config.default_driver = :selenium
 end
 
-def session
-  browser = Capybara.current_session
-  browser.visit URL
-end
 
-session
+browser = Capybara.current_session
+browser.visit URL
+
+#https://engineering.continuity.net/using-ruby-and-capybara-to-scrape/
+
