ae87dd908cc2ba7e28bcdc801043ef047986d3a7
faceroll <set.frnx@gmail.com>
2022-01-04 13:57:50 +0300

    init commit

--- task-6/scraper.rb
+++ task-6/scraper.rb
@@ -1,2 +1,21 @@
+require 'csv'
 require 'selenium-webdriver'
-require 'nokogiri'
+require 'capybara'
+
+URL = 'https://onliner.by'
+
+Capybara.register_driver :selenium do |app|
+  Capybara::Selenium::Driver.new(app, browser: :firefox)
+end
+
+Capybara.javascript_driver = :firefox
+Capybara.configure do |config|
+  config.default_driver = :selenium
+end
+
+def session
+  browser = Capybara.current_session
+  browser.visit URL
+end
+
+session
