15bce6d455078345c1017ab512689e17f2bb92c9
faceroll <set.frnx@gmail.com>
2022-01-05 14:05:48 +0300

    init commit

--- task-6/scraper.rb
+++ task-6/scraper.rb
@@ -3,6 +3,7 @@ require 'selenium-webdriver'
 require 'capybara'
 
 URL = 'https://onliner.by'
+BLOCK = 'ТЕХНОЛОГИИ'
 
 Capybara.register_driver :selenium do |app|
   Capybara::Selenium::Driver.new(app, browser: :firefox)
@@ -13,9 +14,20 @@ Capybara.configure do |config|
   config.default_driver = :selenium
 end
 
-
 browser = Capybara.current_session
 browser.visit URL
 
-#https://engineering.continuity.net/using-ruby-and-capybara-to-scrape/
+browser.all('a', text: BLOCK).first.click
+links = browser.all('a', class: 'news-tiles__stub').map { |item| item['href'] }
 
+result = links.map do |link|
+  browser.visit(link)
+  image = browser.find('div', class: 'news-header__image')['style'].scan(/https:.+\.jpeg/).first
+  title = browser.find('h1').text
+  text = browser.find('.news-text').text[0...200]
+  [image, title, text]
+end
+
+CSV.open('data.csv', 'w') do |csv|
+  result.each { |row| csv << row }
+end
